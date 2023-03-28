2491d5ef423507e9d8f56979fa40191facec6db0
artemijr <arrtemijr@gmail.com>
2023-03-28 09:01:19 +0300

    made help and remove_first commands more user-friendly

--- src/main/kotlin/commands/RemoveFirstCommand.kt
+++ src/main/kotlin/commands/RemoveFirstCommand.kt
@@ -4,7 +4,12 @@ import data.LabWorkCollection
 
 class RemoveFirstCommand(private val labWorkCollection: LabWorkCollection) : Command {
     override fun execute(args: List<Any>): String {
-        return labWorkCollection.removeFirst().toString()
+        val removed = labWorkCollection.removeFirst()
+        return if (removed != null) {
+            "First element removed successfully."
+        } else {
+            "Collection is empty. Cannot remove first element."
+        }
     }
 
     override fun readArguments(readLineFn: () -> String): List<String> {

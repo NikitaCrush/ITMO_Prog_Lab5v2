6348fbcf23aeb74ff87c30591fa07b7415bf79e2
artemijr <arrtemijr@gmail.com>
2023-03-30 09:02:32 +0300

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2" // Add this line for the shadow plugin
 }
 
 group = "com.NikitaCrush"
@@ -11,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation(kotlin("stdlib"))
     implementation("com.google.code.gson:gson:2.10.1")
 }
@@ -26,5 +29,21 @@ tasks {
         kotlinOptions {
             jvmTarget = "1.8"
         }
+    }
+
+    // Add this block to configure the JAR task
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to "MainKt"
+            )
+        }
+        archiveBaseName.set("5Lab")
+        archiveVersion.set("3.5.0")
+    }
+
+
+    withType<ShadowJar> {
+        archiveClassifier.set("")
     }
 }
